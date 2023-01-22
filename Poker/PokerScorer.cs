82810a7623da683e5f3ae71939eb276f7235898d
StevenRRNI <stevenrussell@hotmail.co.uk>
2023-01-22 22:35:21 +0000

    Add comments

--- Poker/PokerScorer.cs
+++ Poker/PokerScorer.cs
@@ -9,6 +9,9 @@ namespace Poker
     {
         private const int HandSize = 5;
 
+        /// <summary>
+        /// Standard list of poker rankings with strongest hand first
+        /// </summary>
         public static List<Hand> DefaultHandRanking = new List<Hand>() 
         {
             new StraightFlush(),
@@ -25,27 +28,46 @@ namespace Poker
 
         private PokerVariant _variant;
 
+        /// <summary>
+        /// Scores poker hands using the default rankings and supplied variant
+        /// </summary>
+        /// <param name="variant">Variant of Poker to be used when calculating hand</param>
         public PokerScorer(PokerVariant variant) : this (variant, DefaultHandRanking)
         {      
         }
 
+        /// <summary>
+        /// Scores poker hands using the supplied variant and ranking list
+        /// </summary>
+        /// <param name="variant">Variant of Poker to be used when calculating hand</param>
+        /// <param name="rankings">List of ordered poker rankings with the strongest hand first</param>
         public PokerScorer(PokerVariant variant, List<Hand> rankings) 
         {
             _variant = variant;
             _rankings = rankings;
         }
 
+        /// <summary>
+        /// Calculates the best hand for given cards
+        /// </summary>
+        /// <param name="own">Cards in the players hand (hole cards)</param>
+        /// <param name="shared">Cards shared between players (community cards)</param>
+        /// <returns>String representation of the best hand possible</returns>
         public string CalculateBestHand(List<PlayingCard> own, List<PlayingCard> shared)
         {
             AssertNoDuplicates(own, shared);
 
             Hand bestHand = null;
 
-            // Calculates all possible 5 card hands base on the variants rules and finds all possible rankings to return the 
+            // Calculates all possible 5 card hands based on the variants rules and finds all possible rankings to return the 
             // highest ranking hand
             //
             // Its very likely this has room for optimization as the rules would mean once some hands are found others are
             // ruled out.
+            //
+            // Another possible way of doing this would have been to go through the best hands possible given all cards and
+            // then rule them out if they didn't using enough cards according to the variants rules but this felt like it
+            // may not have as been as extensible.
             var ownHandCombinations = own.GenerateCombinations(_variant.RequiredHoleCards, _variant.RequiredHoleCards);
 
             var sharedHandCombinations = shared.GenerateCombinations(HandSize - _variant.RequiredHoleCards, HandSize - _variant.RequiredHoleCards);
@@ -60,6 +82,7 @@ namespace Poker
                     {
                         if (hand.HasHand(combination))
                         {
+                            // The rankings position could be pre-calculated instead of always looking it up
                             if (bestHand == null ||
                                 _rankings.IndexOf(hand) < _rankings.IndexOf(bestHand))
                             {
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Poker
+
+## Poker Scorer
+The PokerScorer class is used to calculate the best hand available from a list of own (hole) or shared (community) cards according to the variant supplied in the constructor. The default scoring uses Pokers standard hand rankings which can be overriden by supplying a rankings list to the classes constructor. 
+
+To implement a custom Hand inherit from the Hand class and implement the required methods. Add this hand to a custom ranking lists in the PokerScorer classes constructor.
+
+## Comments
+Hands use a TryGetHand method which returns the cards making up the hand if the hand is possible. This wasn't a requirement but could be used as part of an enhancement to return the cards that make up the best hand. It would also be required for comparing hands to find the best score.
